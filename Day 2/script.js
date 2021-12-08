aac75da951e37998983f669cb06c1668cd29427b
Sorin <jordache_sor@yahoo.com>
2021-12-08 16:03:32 +0200

    made some changes

--- Day 2/script.js
+++ Day 2/script.js
@@ -14,46 +14,40 @@ const spaghetti = {
     image: 'images/plate__spaghetti-meat-sauce.png'
 };
 
+var itemsInCart = {}
+
 const cartItems = document.getElementById("cart-items")
+const emptyCart = document.getElementById("empty-cart")
 const menuFries = document.getElementById("menu-fries")
 const menuSalmon = document.getElementById("menu-salmon")
 const menuSpaghetti = document.getElementById("menu-spaghetti")
 
-menuFries.onclick = function() {
+const totalContainer = document.getElementById("total-container")
+
+menuFries.onclick = function () {
+    HideEmptyCart()
     AddItem(frenchFries);
+    DisableButton(menuFries)
 }
 
-menuSalmon.onclick = function() {
+menuSalmon.onclick = function () {
+    HideEmptyCart()
     AddItem(salmonVegetables);
+    DisableButton(menuSalmon)
 }
 
-menuSpaghetti.onclick = function() {
+menuSpaghetti.onclick = function () {
+    HideEmptyCart()
     AddItem(spaghetti);
+    DisableButton(menuSpaghetti)
 }
 
 function AddItem(menuItem) {
+    itemsInCart[menuItem] = 1
     cartItems.innerHTML += GetCartItemHTML(menuItem)
 }
 
 function GetCartItemHTML(menuItem) {
-    // <div class="cart-item">
-    //     <img class="cart-item-image" src="images/plate__french-fries.png" />
-    //     <div class="cart-item-qty">2</div>
-    //     <div class="cart-item-data">
-    //         <div class="cart-item-name">French Fries with Ketchup</div>
-    //         <div class="cart-item-price">$2.23</div>
-    //         <div class="cart-item-menu">
-    //             <button class="cart-item-btn">
-    //                 <img src="images/chevron.svg" />
-    //             </button>
-    //             <div class="cart-item-menu-qty">2</div>
-    //             <button class="cart-item-btn">
-    //                 <img class="flip" src="images/chevron.svg" />
-    //             </button>
-    //             <div class="cart-item-total">$4.46</div>
-    //         </div>
-    //     </div>
-    // </div>
     return `<div class="cart-item">
                 <img class="cart-item-image" src="${menuItem.image}" />
                 <div class="cart-item-qty">1</div>
@@ -65,11 +59,47 @@ function GetCartItemHTML(menuItem) {
                             <img src="images/chevron.svg" />
                         </button>
                         <div class="cart-item-menu-qty">1</div>
-                        <button class="cart-item-btn">
+                        <button onclick="" class="cart-item-btn">
                             <img class="flip" src="images/chevron.svg" />
                         </button>
                         <div class="cart-item-total">${menuItem.price}</div>
                     </div>
                 </div>
             </div>`
+}
+
+function IncreaseQty(menuItem)
+{
+    UpdateQty(menuItem, itemsInCart[menuItem]+1)
+}
+
+function UpdateQty(menuItem, newValue) {
+    itemsInCart[menuItem] = newValue
+}
+
+function DisableButton(menuButton) {
+    menuButton.classList.remove("menu-item-button")
+    menuButton.classList.add("in-cart-button")
+    menuButton.innerHTML = '<img class="button-icon" src="images/check.svg"></img>In Cart'
+}
+
+function EnableButton()
+{
+    menuButton.classList.add("menu-item-button")
+    menuButton.classList.remove("in-cart-button")
+    menuButton.innerHTML = "Add to Cart"
+}
+
+function ShowEmptyCart()
+{
+    emptyCart.classList.remove("hide")
+    cartItems.classList.add("hide")
+    totalContainer.classList.add("hide")
+}
+
+function HideEmptyCart()
+{
+    emptyCart.classList.add("hide")
+    cartItems.classList.remove("hide")
+    totalContainer.classList.remove("hide")
 }
