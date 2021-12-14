4529089b8d9b1ff000f6cee9e44c5ff1e22bb99c
Sorin <jordache_sor@yahoo.com>
2021-12-14 21:25:13 +0200

    cleaned up code

--- Day 4/script.js
+++ Day 4/script.js
@@ -3,8 +3,6 @@ const keyPressed = document.getElementById("key-pressed")
 const scoreDisplay = document.getElementById("score")
 const buttons = document.querySelectorAll(".key")
 
-//var lastKey
-
 var lastJiggledKey
 var timer = setInterval(JiggleRandomKey, 8000)
 var score = 0
@@ -16,60 +14,20 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 document.addEventListener('keydown', (e) => {
-    // keyDisplay.innerText = `Key pressed: ${e.code}`
     if (e.key === "Tab") {
         e.preventDefault()
     }
 
     CompareKeys(e.code)
-    // if(lastJiggledKey != undefined)
-    // {
-    //     // keyPressed.innerText = `Key data: ${lastJiggledKey.getAttribute("data-key")}`
-    //     if (e.code == lastJiggledKey.getAttribute("data-key")) {
-    //         UnjiggleKey(lastJiggledKey)
-    //         lastJiggledKey = undefined
-    //         score++
-    //     }
-    //     else
-    //     {
-    //         score--
-    //     }
-
-    //     UpdateScore()
-    // }
 })
-
-// document.addEventListener('keyup', (e) => {
-//     // keyDisplay.innerText = `Key pressed: ${e.code}`
-//     if(lastJiggledKey != undefined)
-//     {
-//         if (e.key == lastJiggledKey.getAttribute("data-key")) {
-//             UnjiggleKey(lastJiggledKey)
-//             lastJiggledKey = undefined
-//         }
-//     }
-// })
 
 function MouseClickHandler(e) {
     CompareKeys(e.target.getAttribute("data-key"))
-    //keyPressed.innerText = e.target.getAttribute("data-key")
-    // keyPressed.innerText = `Key clicked: ${e.target.getAttribute("data-key")}`
-    //e.target.setAttribute("data-key", lastKey)
-
-    // if (e.target.classList.contains("jiggle"))
-    // {
-    //     UnjiggleKey(e.target)
-    // }
-    // else
-    // {
-    //     JiggleKey(e.target)
-    // }
 }
 
 function CompareKeys(keyCode) {
     if(lastJiggledKey != undefined)
     {
-        // keyPressed.innerText = `Key data: ${lastJiggledKey.getAttribute("data-key")}`
         if (keyCode == lastJiggledKey.getAttribute("data-key")) {
             UnjiggleKey(lastJiggledKey)
             lastJiggledKey = undefined
